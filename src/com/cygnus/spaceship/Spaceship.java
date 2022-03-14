5084bd9c937f31854cdbaf4a42903582df0689d9
Chuck439 <firmankediri123@gmail.com>
2022-03-14 12:10:27 +0000

    Spaceship:  Remove UI Background
    * Let it follow monet theme
    
    Signed-off-by: Chuck439 <firmankediri123@gmail.com>

--- src/com/cygnus/spaceship/Spaceship.java
+++ src/com/cygnus/spaceship/Spaceship.java
@@ -35,22 +35,6 @@ public class Spaceship extends SettingsPreferenceFragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        int currentNightMode = getContext().getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-        switch (currentNightMode) {
-            case Configuration.UI_MODE_NIGHT_NO:
-                // Night mode is not active, we're using the light theme
-                getListView().setBackgroundResource(R.drawable.spaceship_background);
-                break;
-            case Configuration.UI_MODE_NIGHT_YES:
-                // Night mode is active, we're using dark theme
-                getListView().setBackgroundResource(R.drawable.spaceship_background_dark);
-                break;
-        }
-    }
-
-    @Override
     public int getMetricsCategory() {
         return MetricsProto.MetricsEvent.SPACESHIP;
     }
